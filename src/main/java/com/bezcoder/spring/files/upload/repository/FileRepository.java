ef0b56af3cc3a95f8c21fbec0ef157f18351eaca
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2023-01-30 17:26:32 +0300

    Add FileRepository

---
+++ src/main/java/com/bezcoder/spring/files/upload/repository/FileRepository.java
@@ -0,0 +1,13 @@
+package com.bezcoder.spring.files.upload.repository;
+
+import com.bezcoder.spring.files.upload.model.FileInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface FileRepository extends MongoRepository<FileInfo, String> {
+    Optional<FileInfo> findFileByFileName(String name);
+}
